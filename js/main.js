34de899196d3384b720b7e487719afead984a04b
Nathaniel Miller <n.william.m@gmail.com>
2014-06-11 15:28:06 -0400

    Initial commit.
    Initial project files commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(".happy-side").hover(
+	function() {
+		$(".happy-text").css("color", "#78FF78");
+	}
+	, function() {
+		$(".happy-text").css("color", "#5BC15B");
+	}
+);
+
+$(".sad-side").hover(
+	function() {
+		$(".sad-text").css("color", "#9F74FF");
+	}
+	, function() {
+		$(".sad-text").css("color", "#7757BF");
+	}
+);
+
+$(".happy-side").click(
+	function() {
+		window.location.href="/HappyOrSad/happy.html";
+	}
+);
+
+$(".sad-side").click(
+	function() {
+		window.location.href="/HappyOrSad/sad.html";
+		
+	}
+);
